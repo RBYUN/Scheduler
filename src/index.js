cb1427ee45895d28e7b7319046e6df460e083df0
Rodney Byun <rjbyun@gmail.com>
2025-11-11 17:06:42 -0500

    init

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const cors = require('cors')
+
+const app = express();
+
+app.use(express.json());
+
+app.listen(3000, () => {
+    console.log('Server started listening on port: 3000')
+});
