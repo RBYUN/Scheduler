df5b9662e324edc2e8119c3f6973a645d68d6183
Rodney Byun <rjbyun@gmail.com>
2025-11-11 21:53:37 -0500

    added basic routing

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ const cors = require('cors')
 const app = express();
 
 app.use(express.json());
+app.use(cors());
+
+app.use(require('./routes'));
 
 app.listen(3000, () => {
     console.log('Server started listening on port: 3000')
